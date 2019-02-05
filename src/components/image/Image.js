3f92e1036b1b0e5f0bb3ee50fe367ecf936e884d
vinicius <vinicius@tootz.com.br>
2019-02-05 08:41:38 -0300

    Updates

--- src/components/image/Image.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import './image.scss';
-
-export const Image = (props) => {
-  return (
-      <img alt={props.alt} src={require('../../images/'+props.source)} />
-  )
-}
